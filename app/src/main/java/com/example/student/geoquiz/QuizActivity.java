3d115c1c2481e7f9589147ff26512581807ef714
MonteSmith <MonteSmithJr@yahoo.com>
2015-09-30 19:24:37 -0700

    This is Lab 2

--- app/src/main/java/com/example/student/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/student/geoquiz/QuizActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.geoquiz;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,63 +14,114 @@ import android.widget.Toast;
 import com.example.student.TrueFalse;
 
 
-public class QuizActivity extends ActionBarActivity {
-
+public class QuizActivity extends Activity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
-        new TrueFalse(R.string.question_oceans, true),
-        new TrueFalse(R.string.question_mideast, false),
-        new TrueFalse(R.string.question_africa, false),
-        new TrueFalse(R.string.question_americas, true),
-        new TrueFalse(R.string.question_asia, true),
-};
-private int mCurrentIndex = 0;
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+    private int mCurrentIndex = 0;
+    private TrueFalse[] mAnswerKey;
 
-    private void updateQuestion(){
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
+    private void updateQuestion() {
+        int question = mAnswerKey[mCurrentIndex].getQuestion();
         mQuestionTextView.setText(question);
     }
+
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
         int messageRedId;
 
-         if (userPressedTrue == answerIsTrue) {
-             messageRedId = R.string.correct_toast;
-         } else {
-             messageRedId = R.string.incorrect_toast;
-         }
+        if (userPressedTrue == answerIsTrue) {
+            messageRedId = R.string.correct_toast;
+        } else {
+            messageRedId = R.string.incorrect_toast;
+        }
         Toast.makeText(this, messageRedId, Toast.LENGTH_SHORT);
     }
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_quiz);
-    mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-    mTrueButton = (Button) findViewById(R.id.true_button);
-    mFalseButton = (Button) findViewById(R.id.false_button);
 
-    mTrueButton.setOnClickListener(new View.OnClickListener() {
-        @Override
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_quiz);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
 
-        public void onClick(View v) {
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                Log.d(TAG, "onStart() called");
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                Log.d(TAG, "onPause() called");
+            }
+
+            @Override
+            public void onResume() {
+                super.onResume();
+                Log.d(TAG, "onResume() called");
+            }
+
+            @Override
+            public void onStop() {
+                super.onStop();
+                Log.d(TAG, "onStop() called");
+            }
+
+            @Override
+            public void onDestroy() {
+                super.onDestroy();
+                Log.d(TAG, "onDestroy() called");
+            }
+
+
+            @Override
+
+            public void onClick(View v) {
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
-    });
+        updateQuestion();
+    }
 
-    mNextButton = (Button)findViewById(R.id.next_button);
-    mNextButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-        }
-    });
-    updateQuestion();
-
-
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+    {
     mFalseButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -78,10 +131,6 @@ protected void onCreate(Bundle savedInstanceState) {
 
 
 }
-
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/student/geoquiz/R.java
@@ -0,0 +1,7 @@
+package com.example.student.geoquiz;
+
+/**
+ * Created by student on 9/28/2015.
+ */
+public interface R {
+}
